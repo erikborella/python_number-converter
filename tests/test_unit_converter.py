a4771f4726888d0a8d4032f8dc917674c5479ff6
Erik <erikborella@gmail.com>
2019-06-22 14:47:53 -0300

    More tests

--- tests/test_unit_converter.py
+++ tests/test_unit_converter.py
@@ -17,5 +17,53 @@ class TestGetFractionalPart(unittest.TestCase):
         self.assertEqual(5, unit_converter.get_fractional_part(0.5))
 
 
+class TestIntegerDecimalToAnyBase(unittest.TestCase):
+    def test_binary_convert(self):
+        self.assertEqual('1010', unit_converter.integer_decimal_to_any_base(10, 2))
+        self.assertEqual('1010', unit_converter.integer_decimal_to_any_base(10.1, 2))
+        self.assertEqual('11111111', unit_converter.integer_decimal_to_any_base(255, 2))
+
+    def test_octal_convert(self):
+        self.assertEqual('12', unit_converter.integer_decimal_to_any_base(10, 8))
+        self.assertEqual('12', unit_converter.integer_decimal_to_any_base(10.1, 8))
+        self.assertEqual('377', unit_converter.integer_decimal_to_any_base(255, 8))
+
+    def test_decimal_convert(self):
+        self.assertEqual('10', unit_converter.integer_decimal_to_any_base(10, 10))
+        self.assertEqual('10', unit_converter.integer_decimal_to_any_base(10.1, 10))
+        self.assertEqual('255', unit_converter.integer_decimal_to_any_base(255, 10))
+
+    def test_hexadecimal_convert(self):
+        self.assertEqual('A', unit_converter.integer_decimal_to_any_base(10, 16))
+        self.assertEqual('A', unit_converter.integer_decimal_to_any_base(10.1, 16))
+        self.assertEqual('FF', unit_converter.integer_decimal_to_any_base(255, 16))
+
+
+class TestFloatingDecimalToAnyBase(unittest.TestCase):
+    def test_binary_convert(self):
+        self.assertEqual('1010.0', unit_converter.floating_decimal_to_any_base(10, 2))
+        self.assertEqual('1010.0', unit_converter.floating_decimal_to_any_base(10.0, 2))
+        self.assertEqual('1010.1', unit_converter.floating_decimal_to_any_base(10.10, 2))
+        self.assertEqual('11111111.11111111', unit_converter.floating_decimal_to_any_base(255.255, 2))
+
+    def test_octal_convert(self):
+        self.assertEqual('12.0', unit_converter.floating_decimal_to_any_base(10, 8))
+        self.assertEqual('12.0', unit_converter.floating_decimal_to_any_base(10.0, 8))
+        self.assertEqual('12.1', unit_converter.floating_decimal_to_any_base(10.1, 8))
+        self.assertEqual('377.377', unit_converter.floating_decimal_to_any_base(255.255, 8))
+
+    def test_decimal_convert(self):
+        self.assertEqual('10.0', unit_converter.floating_decimal_to_any_base(10, 10))
+        self.assertEqual('10.0', unit_converter.floating_decimal_to_any_base(10.0, 10))
+        self.assertEqual('10.1', unit_converter.floating_decimal_to_any_base(10.1, 10))
+        self.assertEqual('255.255', unit_converter.floating_decimal_to_any_base(255.255, 10))
+
+    def test_hexadecimal_convert(self):
+        self.assertEqual('A.0', unit_converter.floating_decimal_to_any_base(10, 16))
+        self.assertEqual('A.0', unit_converter.floating_decimal_to_any_base(10.0, 16))
+        self.assertEqual('A.1', unit_converter.floating_decimal_to_any_base(10.1, 16))
+        self.assertEqual('FF.FF', unit_converter.floating_decimal_to_any_base(255.255, 16))
+
+
 if __name__ == '__main__':
     unittest.main()
